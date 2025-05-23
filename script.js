e444cd5fbb603c98c10307f2cdaaf5eae8a3def2
salmaturky <slmytrly214@gmail.com>
2025-05-23 13:44:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+const header = document.getElementById('main-header');
+const scrollThreshold = 50;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > scrollThreshold) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
